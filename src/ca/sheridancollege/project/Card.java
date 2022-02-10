6226e29b8633560bea084a7d219230420ead5496
soni-z <97900417+soni-z@users.noreply.github.com>
2022-02-10 13:43:46 -0500

    Update Card.java
    modified.

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -7,20 +7,57 @@ package ca.sheridancollege.project;
 
 /**
  * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code 
- * should remember to add themselves as a modifier.
+ * enough to be instantiated for any Card game. Students wishing to add to the
+ * code should remember to add themselves as a modifier.
+ *
  * @author dancye, 2018
+ * @modifier data pirates
  */
-public abstract class Card 
-{
+public abstract class Card {
+
+    public enum Colors {
+        Red, Yellow, Green, Blue
+    };
+
+    public enum Ranks {
+        ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, SKIP, REVERSE, DRAWTWO, DRAWFOUR, WILDCARD
+    };
+    private final Colors colour;
+    private final Ranks rank;
     //default modifier for child classes
-    
+
     /**
-     * Students should implement this method for their specific children classes 
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     * Students should implement this method for their specific children classes
+     *
+     * @return a String representation of a card. Could be an UNO card, a
+     * regular playing card etc.
      */
-    
+    /*
+        We have taken the reference from the inclass assignment of uno game.
+        Source Inclass Assigement ICE02
+     */
+    public Card(Colors c, Ranks r) {
+        colour = c;
+        rank = r;
+    }
+
+    public Ranks getValue() {
+        return this.rank;
+    }
+
+    public Colors getColors() {
+        return this.colour;
+    }
+
     @Override
     public abstract String toString();
-    
+
 }
+
+//default modifier for child classes
+/**
+ * Students should implement this method for their specific children classes
+ *
+ * @return a String representation of a card. Could be an UNO card, a regular
+ * playing card etc.
+ */
